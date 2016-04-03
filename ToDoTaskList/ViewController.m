6f23a71eaf7c1a15c39b0eff5ae4882c8406481e
Alextnelson <alexander.thomas.nelson@gmail.com>
2016-04-03 16:26:18 -0500

    Add initial framework for app

--- ToDoTaskList/ViewController.m
+++ ToDoTaskList/ViewController.m
@@ -8,7 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate>
+
+@property NSMutableArray *tasks;
+@property (weak, nonatomic) IBOutlet UITextField *addTaskTextField;
+@property (weak, nonatomic) IBOutlet UITableView *tasksTableView;
 
 @end
 
@@ -16,12 +20,78 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.tasks = [[NSMutableArray alloc] initWithObjects:
+    @"Go to the grocery store",
+    @"Buy books at university store",
+    @"Grab toothpaste",
+    @"Place a stop on mail",
+    nil];
+    self.navigationItem.rightBarButtonItem = self.editButtonItem;
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - TableView Editing
+
+- (void)setEditing:(BOOL)editing animated:(BOOL)animated {
+    [super setEditing:editing animated:animated];
+    [self.tasksTableView setEditing:editing animated:YES];
 }
+
+- (IBAction)onAddButtonPressed:(id)sender {
+    [self.tasks addObject:self.addTaskTextField.text];
+    self.addTaskTextField.text = @"";
+    [self.tasksTableView reloadData];
+    [self.addTaskTextField resignFirstResponder];
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Delete" message:@"Are you sure?" preferredStyle:UIAlertControllerStyleAlert];
+
+        UIAlertAction *cancel = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        }];
+
+        UIAlertAction *delete = [UIAlertAction actionWithTitle:@"Delete" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
+            [self.tasks removeObjectAtIndex:indexPath.row];
+            [self.tasksTableView reloadData];
+        }];
+        [alert addAction:cancel];
+        [alert addAction:delete];
+        [self presentViewController:alert animated:YES completion:nil];
+    }
+}
+
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return YES;
+}
+
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath
+{
+    NSString *stringToMove = self.tasks[sourceIndexPath.row];
+    [self.tasks removeObjectAtIndex:sourceIndexPath.row];
+    [self.tasks insertObject:stringToMove atIndex:destinationIndexPath.row];
+}
+
+#pragma mark - TableView Data Source
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.tasks.count;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"taskID"];
+    cell.textLabel.text = [self.tasks objectAtIndex:indexPath.row];
+
+    return cell;
+    
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    cell.textLabel.textColor = [UIColor greenColor];
+    cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
 
 @end
